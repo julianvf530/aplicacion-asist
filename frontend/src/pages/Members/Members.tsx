a9ad9780f9406b52dba79bf9bcce84c81b140517
julvilfer1 <julvilfer1@alum.us.es>
2026-07-20 14:14:04 +0200

    primer filto

--- frontend/src/pages/Members/Members.tsx
+++ frontend/src/pages/Members/Members.tsx
@@ -1,13 +1,30 @@
 import { members } from "../../data/members";
 import MemberCard from "../../components/MemberCard";
+import { useState } from "react";
+import type { ChangeEvent } from "react";
+
 
 export default function Members(){
-    
+    const [text,setText] =useState("");
+   
+    const manejo = (event: ChangeEvent<HTMLInputElement>) => {
+        setText(event.target.value);
+    }
+
+    const filteredMembers = members.filter((member) => 
+        member.nombre.toLowerCase().includes(text.toLowerCase())
+    )
+
     return (
         <div>
             <h1>Miembros</h1>
+            <input
+                type="text"
+                value={text}
+                onChange= {manejo}
+            />
 
-            {members.map((member) =>
+            {filteredMembers.map((member) =>
 
                 <MemberCard
                     key={member.id}
