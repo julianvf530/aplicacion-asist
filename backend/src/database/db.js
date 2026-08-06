e4a0490f04835c7ab76e425333db1db25bab82b8
julvilfer1 <julvilfer1@alum.us.es>
2026-08-06 13:21:32 +0200

    volumenes

--- backend/src/database/db.js
+++ backend/src/database/db.js
@@ -1,12 +1,13 @@
 const path = require("path");
 const Database = require("better-sqlite3");
 
-const dbPath = path.join(__dirname, "database.sqlite");
+const dbPath = process.env.DB_PATH ||
+    path.join(__dirname, "database.sqlite");
 
 const db = new Database(dbPath);
 
 db.pragma("foreign_keys = ON");
 
-console.log("SQLite conectado");
+console.log(`SQLite conectado: ${dbPath}`);
 
 module.exports = db;
